dd63e377aea40d0c0111faccc35cde02fbeb1289
pVm <135732027+varun2482@users.noreply.github.com>
2024-08-25 12:58:01 +0530

    Update 1st.cpp

--- 1st.cpp
+++ 1st.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-  int a;
-  cout<<"enter a no."<<endl;
-  cin>>a;
-  std::cout<<"hello world";
+  //int a;    integer datatypes
+  //cout<<"enter a no."<<endl;
+  //cin>>a;
+  std::cout<<"hello world";//you can ignore "std::" as it was mainly refer to old systems.
 }
