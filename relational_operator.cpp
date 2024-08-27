a7784cf22a3b594971aaab6b4ae211fbf60c65b7
pVm <135732027+varun2482@users.noreply.github.com>
2024-08-27 19:26:32 +0530

    Add files via upload

---
+++ relational_operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a=5,b=7;
+  //add  explanation about relational operator.
+  bool first=(a==b);
+  cout<<first<<endl;
+  bool second=(a!=b);
+  cout<<second<<endl;
+  bool third=(a>=b);
+  cout<<third<<endl;
+  bool fourth=(a<=b);
+  cout<<fourth<<endl;
+  bool fifth=(a>b);
+  cout<<fifth<<endl;
+  bool sixth=(a<b);
+  cout<<sixth<<endl;
+}
