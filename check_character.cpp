7801ceb5145c3c71989506910d12ef3640678717
pVm <135732027+varun2482@users.noreply.github.com>
2024-08-27 19:12:19 +0530

    Add files via upload

---
+++ check_character.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+  char ch;
+  cout << "Enter a character: "<<endl;
+  cin >> ch;
+  if(ch>='a' && ch<='z'){
+    cout << "The character is a lowercase letter." << endl;
+  }
+  else if(ch>='A' && ch<='Z'){
+    cout << "The character is an uppercase letter." << endl;
+  }
+  else if(ch>='0' && ch<='9'){
+    cout << "The character is a digit." << endl;
+  }
+  else{
+    cout<<"it is special character";
+  }
+}
