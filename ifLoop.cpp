5cab5f3ff3df98a1ee40bdad94a4c8fc9d77407c
pVm <135732027+varun2482@users.noreply.github.com>
2024-08-28 18:54:42 +0530

    Add files via upload

---
+++ ifLoop.cpp
@@ -0,0 +1,42 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+  int num;
+  cout << "Enter a number: ";
+  cin >> num;
+  if (num>0) 
+  {
+    cout << "The number is positive." << endl;
+  }
+  else if(num=0)
+  {
+    cout << "The number is negative." << endl;
+  }
+  else
+  {
+    cout << "The number is zero." << endl;
+  }
+}
+*/
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int num=24;
+// cout<<"Enter a number: ";
+// cin>>num;
+if(num>0){
+  cout<<"given number is positive"<<endl;
+}
+else if(num==24){
+cout<<"given number is 24"<<endl;
+}
+else{
+cout<<"number is negative";
+}
+}
