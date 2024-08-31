c85ab29858bc0944b9c4f5a14dceba92a11dac3b
pVm <135732027+varun2482@users.noreply.github.com>
2024-08-31 18:40:09 +0530

    Update 7-pattern.cpp

--- 7-pattern.cpp
+++ 7-pattern.cpp
@@ -3,22 +3,22 @@
 
 
 //      1st
-// #include<iostream>
-// using namespace std;
-// int main() {
-// int n, row=1;
-// cout << "Enter the number of elements: ";
-// cin >> n;
-// while(row<=n){
-//   int col=1;
-//   while(col<=n){
-//     cout << " * ";
-//     col++;
-//   }
-//   cout << endl;
-//   col++;
-// }
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+ int n, row=1;
+ cout << "Enter the number of elements: ";
+ cin >> n;
+ while(row<=n){
+   int col=1;
+   while(col<=n){
+     cout << " * ";
+     col++;
+   }
+   cout << endl;
+   col++;
+ }
+ }
 
 //above pattern will give a 
 // * * *
@@ -29,67 +29,66 @@
 
 
 //      2nd
-// #include<iostream>
-// using namespace std;
-// int main() {
-//   int n, i=1;
-//   cout << "Enter the number of elements: ";
-//   cin >> n;
-//   while (i<=n){
-//     int j=1;
-//     while (j<=n){
-//       cout<<j;
-//       j++;
-//     }
-//     cout<<endl;
-//     i++;
-//   }
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+   int n, i=1;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   while (i<=n){
+     int j=1;
+     while (j<=n){
+       cout<<j;
+       j++;
+     }
+     cout<<endl;
+     i++;
+   }
+ }
 
 
 
 //        3rd
-// #include <iostream>
-// using namespace std;
+ #include <iostream>
+ using namespace std;
+ int main() {
+     int n,i = 1;  // Initialize row variable
+     cout << "Enter the number of elements: ";
+     cin >> n;
+     while (i <= n) {  // Outer loop to control rows
+         int j = 0;  // Initialize column variable
+         while (j < n) {  // Inner loop to print 4 times each row
+             cout << i << " ";  // Print the value of i
+             j++;  // Increment column counter
+         }
+         cout << endl;  // New line after each row
+         i++;  // Increment row counter
+     }
+     return 0;
+ }
 
-// int main() {
-//     int n,i = 1;  // Initialize row variable
-//     cout << "Enter the number of elements: ";
-//     cin >> n;
-//     while (i <= n) {  // Outer loop to control rows
-//         int j = 0;  // Initialize column variable
-//         while (j < n) {  // Inner loop to print 4 times each row
-//             cout << i << " ";  // Print the value of i
-//             j++;  // Increment column counter
-//         }
-//         cout << endl;  // New line after each row
-//         i++;  // Increment row counter
-//     }
-//     return 0;
-// }
 
 
 
 
 //        4th
-// #include<iostream>
-// using namespace std;
-// int main() {
-//   int n,i=1, count=1;
-//   cout<<"enter a number: "<<endl;
-//   cin>>n;
-//   while(i<=n){
-//     int j=1;
-//     while(j<=n){
-//       cout<<count<<" ";
-//       j++;
-//       count++;
-//     }
-//     cout<<endl;
-//     i++;
-//   }
-    
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+   int n,i=1, count=1;
+   cout<<"enter a number: "<<endl;
+   cin>>n;
+   while(i<=n){
+     int j=1;
+     while(j<=n){
+       cout<<count<<" ";
+       j++;
+       count++;
+     }
+     cout<<endl;
+     i++;
+   }
+ }
 
 
 
@@ -98,87 +97,87 @@
 //            5th
 //here we are taking pattern as a right angle triangle(number or star.)
 
-// #include<iostream>
-// using namespace std;
-// int main() {
-//   int n,i=1;
-//   cout<<"enter a number: "<<endl;
-//   cin>>n;
-//   while(i<=n){
-//     int j=1;
-//     while(j<=i){
-//       cout<<" * ";
-//       j++;
-//       }
-//       cout<<endl;
-//       i++;
-//   }
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+   int n,i=1;
+   cout<<"enter a number: "<<endl;
+   cin>>n;
+   while(i<=n){
+     int j=1;
+     while(j<=i){
+       cout<<" * ";
+       j++;
+       }
+       cout<<endl;
+       i++;
+   }
+ }
 
 
 
 
 
 //        6th
-// #include<iostream>
-// using namespace std;
-// int main() {
-//   int n,i=1;
-//   cout<<"enter a number: "<<endl;
-//   cin>>n;
-//   while(i<=n){
-//     int j=1;
-//     while(j<=i){
-//       cout<<i<<" ";
-//       j++;
-//       }
-//       cout<<endl;
-//       i++;
-//   }
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+   int n,i=1;
+   cout<<"enter a number: "<<endl;
+   cin>>n;
+   while(i<=n){
+     int j=1;
+     while(j<=i){
+       cout<<i<<" ";
+       j++;
+       }
+       cout<<endl;
+       i++;
+   }
+ }
 
 
 
 
 
 //        7th
-// #include<iostream>
-// using namespace std;
-// int main() {
-//   int n,i=1;
-//   cout<<"enter a number: "<<endl;
-//   cin>>n;
-//   while(i<=n){
-//     int j=1;
-//     while(j<=i){
-//       cout<<i+j-1<<" ";
-//       j++;
-//       }
-//       cout<<endl;
-//       i++;
-//   }
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+   int n,i=1;
+   cout<<"enter a number: "<<endl;
+   cin>>n;
+   while(i<=n){
+     int j=1;
+     while(j<=i){
+       cout<<i+j-1<<" ";
+       j++;
+       }
+       cout<<endl;
+       i++;
+   }
+ }
 
 
 
 
 //        8th
-// #include<iostream>
-// using namespace std;
-// int main() {
-//   int n,i=1;
-//   cout<<"enter a number: "<<endl;
-//   cin>>n;
-//   while(i<=n){
-//     int j=1;
-//     while(j<=i){
-//       cout<<i-j+1<<" ";
-//       j++;
-//       }
-//       cout<<endl;
-//       i++;
-//   }
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+   int n,i=1;
+   cout<<"enter a number: "<<endl;
+   cin>>n;
+   while(i<=n){
+     int j=1;
+     while(j<=i){
+       cout<<i-j+1<<" ";
+       j++;
+       }
+       cout<<endl;
+       i++;
+   }
+ }
 
 
 
