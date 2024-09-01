263c64299f34e3e3199d135774fd7c4cde58a572
pVm <135732027+varun2482@users.noreply.github.com>
2024-09-01 09:08:29 +0530

    Update 6-while.cpp

--- 6-while.cpp
+++ 6-while.cpp
@@ -1,42 +1,46 @@
-//easy while loop conditions will be taken here
+//    while loop
 
-//  printing numbers sequentially upto n 
-
-//  #include<iostream>
-//  using namespace std;
-//  int main() {
-//    int num , i=1;
-//    cout<<"Enter a number: ";
-//    cin>>num;
-//    while(i<=num) {
-//      cout<<i<<"\n";
-//      i++;
-//      }
-//      return 0;
-//  }
-
-
+// printing numbers sequentially upto n 
+//    1st
+  #include<iostream>
+  using namespace std;
+  int main() {
+    int num , i=1;
+    cout<<"Enter a number: ";
+    cin>>num;
+    while(i<=num) {
+      cout<<i<<"\n";
+      i++;
+      }
+      return 0;
+  }
 
 
 
 
+
+
+//    2nd
 // print sum of first n numbers 
 
-// #include<iostream>
-// using namespace std;
-// int main() {
-//   int num , i=1 , sum=0;
-//   cout<<"enter a number "<<endl;
-//   cin>>num;
-//   while(i<=num){
-//     sum=sum+i;
-//     i++;
-//   }
-//   cout<<"sum of first "<<num<<" natural numbers is "<<sum<<endl;
-// }
+ #include<iostream>
+ using namespace std;
+ int main() {
+   int num , i=1 , sum=0;
+   cout<<"enter a number "<<endl;
+   cin>>num;
+   while(i<=num){
+     sum=sum+i;
+     i++;
+   }
+   cout<<"sum of first "<<num<<" natural numbers is "<<sum<<endl;
+ }
 
 
 
+
+
+//      3rd
 //sum of all even numbers
 #include<iostream>
 using namespace std;
@@ -52,7 +56,3 @@ int main() {
   }
   cout<<"sum of all even numbers till "<<num <<"is "<<sum<<endl;
 }  
-
-
-
-
