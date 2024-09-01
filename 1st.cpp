9fbd4c4850112be1ecd81f7d0979663a079262b2
pVm <135732027+varun2482@users.noreply.github.com>
2024-09-01 09:02:10 +0530

    Update 1st.cpp

--- 1st.cpp
+++ 1st.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main()
 {
-  //int a;    integer datatypes
+  //int a;   // integer datatypes
   //cout<<"enter a no."<<endl;
-  //cin>>a;
+  //cin>>a;  //console used for user interaction(saved in std namespace)
   std::cout<<"hello world";//you can ignore "std::" as it was mainly refer to old systems.
 }
