5df084960aae929bbb4c90ea4be13b8a2a868067
pVm <135732027+varun2482@users.noreply.github.com>
2024-09-04 19:56:57 +0530

    Update 8-bitwise_operator.cpp

--- 8-bitwise_operator.cpp
+++ 8-bitwise_operator.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
 using namespace std;
-int main(){
-  int a,b;
-  cout<<"enter 2 numbers: "<<endl;
-  cin>>a>>b;
 
-  cout<<"a&b"<<(a&b)<<endl;
-  cout<<"a|b"<<(a|b)<<endl;
-  cout<<"~a"<<~a<<endl;
-  cout<<"a^b"<<(a^b)<<endl;
+int main() {
+    int a, b;
+    
+    // Input two integers
+    cout << "Enter 2 numbers: " << endl;
+    cin >> a >> b;
 
+    // Perform bitwise AND operation
+    cout << "a & b = " << (a & b) << endl;    
+
+    // Perform bitwise OR operation
+    cout << "a | b = " << (a | b) << endl;
+
+    // Perform bitwise NOT operation on 'a'
+    cout << "~a = " << (~a) << endl;
+
+    // Perform bitwise XOR operation
+    cout << "a ^ b = " << (a ^ b) << endl;
+
+    // Left shift a by 1
+    cout << "a << 1 = " << (a << 1) << endl;
+
+    // Right shift b by 1
+    cout << "b >> 1 = " << (b >> 1) << endl;
+
+    return 0;
 }
