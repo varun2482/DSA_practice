a085403ecbf247608026c997bb1b132f5fc8ee8c
pVm <135732027+varun2482@users.noreply.github.com>
2024-09-04 19:49:53 +0530

    Add files via upload

---
+++ 8-bitwise_operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"enter 2 numbers: "<<endl;
+  cin>>a>>b;
+
+  cout<<"a&b"<<(a&b)<<endl;
+  cout<<"a|b"<<(a|b)<<endl;
+  cout<<"~a"<<~a<<endl;
+  cout<<"a^b"<<(a^b)<<endl;
+
+}
