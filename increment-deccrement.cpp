5692f787b26a813d5791df5abcc79d102dc1998c
pVm <135732027+varun2482@users.noreply.github.com>
2024-09-15 21:48:18 +0530

    Create increment-deccrement.cpp

---
+++ increment-deccrement.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<(i++)<<endl;        //this is post-increment operator
+    cout<<(++i)<<endl;        //this is pre-increment operator
+    cout<<(i--)<<endl;        //this is post-decrement operator
+    cout<<(--i)<<endl;        //this is pre-decrement operator
+}
+
+
+
+
+//----------------------------------------------Examples---------------------------------------------
