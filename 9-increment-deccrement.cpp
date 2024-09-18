59aa38bd8de66352247de4ccbe72f2eb5264ea9f
pVm <135732027+varun2482@users.noreply.github.com>
2024-09-18 20:01:00 +0530

    Update 9-increment-deccrement.cpp

--- 9-increment-deccrement.cpp
+++ 9-increment-deccrement.cpp
@@ -12,3 +12,14 @@ int main()
 
 
 //----------------------------------------------Examples---------------------------------------------
+//        1st
+#include<iostream>
+using namespace std;
+int main()    {
+    int a, b=5;
+    a=10;
+    if(++a)
+        cout<<b;
+    else
+        cout<<++b;
+}
